58d8a96a97f9f88e6b7a077b899f915ba74e60d2
UTOQINGAPP <utoqingapp@gmail.com>
2024-09-13 23:34:11 -0500

    support web fix

--- lib/ui/layouts/authentication/authentication_layout.dart
+++ lib/ui/layouts/authentication/authentication_layout.dart
@@ -2,39 +2,45 @@ import 'package:chat/configs/configs.dart';
 import 'package:flutter/foundation.dart';
 export 'views/views_authentication.dart';
 
-class AuthenticationLayout extends StatelessWidget {
+class AuthenticationLayout extends HookWidget {
   final StatefulNavigationShell navigationShell;
   const AuthenticationLayout({super.key, required this.navigationShell});
 
   @override
   Widget build(BuildContext context) {
+    ValueNotifier<double> widthWeb = useState(context.width(50));
+    final ChangeSizeScreenHelperConfig changeSizeScreenHelperConfig =
+        ChangeSizeScreenHelperConfig();
+    if (changeSizeScreenHelperConfig.isLargeScreen(context)) {
+      widthWeb.value = context.width(50);
+    } else if (changeSizeScreenHelperConfig.isMediumScreen(context)) {
+      widthWeb.value = context.width(70);
+    } else if (changeSizeScreenHelperConfig.isSmallScreen(context)) {
+      widthWeb.value = context.width(100);
+    }
     return Scaffold(
-      extendBody: true,
       body: Center(
         child: SingleChildScrollView(
-          child: SizedBox(
-            height: context.height(100),
-            width: kIsWeb ? context.width(50) : context.width(100),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                if (!kIsWeb) const Spacer(),
-                const _LogoViewAuthentication(),
-                if (!kIsWeb) navigationShell,
-                if (kIsWeb) Expanded(child: navigationShell),
-                if (!kIsWeb) const Spacer(),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    margin: const EdgeInsets.only(bottom: 20),
-                    child: const Text(
-                      'Términos y condiciones de uso',
-                      style: TextStyle(fontWeight: FontWeight.w200),
+          child: Center(
+            child: SizedBox(
+              width: kIsWeb ? widthWeb.value : context.width(100),
+              child: Column(
+                children: [
+                  const _LogoViewAuthentication(),
+                  navigationShell,
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      margin: const EdgeInsets.only(bottom: 20),
+                      child: const Text(
+                        'Términos y condiciones de uso',
+                        style: TextStyle(fontWeight: FontWeight.w200),
+                      ),
                     ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -61,7 +67,7 @@ class _LogoViewAuthentication extends StatelessWidget {
             const Text(
               'Messenger',
               style: TextStyle(fontSize: 30),
-            )
+            ),
           ],
         ),
       ),
--- lib/ui/layouts/authentication/views/register/register_view_authentication.dart
+++ lib/ui/layouts/authentication/views/register/register_view_authentication.dart
@@ -4,6 +4,7 @@ import 'package:chat/ui/layouts/authentication/views/login/login_view_authentica
 import 'package:chat/ui/layouts/authentication/views/register/logic/logic_register.dart';
 import 'package:chat/ui/layouts/home/home_layout.dart';
 import 'package:chat/ui/shared/shared_ui.dart';
+import 'package:flutter/foundation.dart';
 
 class RegisterViewAuthentication extends ConsumerWidget {
   static String link = '/authentication/register';
@@ -13,72 +14,67 @@ class RegisterViewAuthentication extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     FormLogicStateRegister formLogicStateRegister =
         ref.watch(formLogicRegisterProvider);
-    return Container(
-      margin: const EdgeInsets.only(top: 40),
-      padding: const EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        children: [
-          CustomInputComponentShared(
-            icon: Icons.person,
-            placeholder: 'Nombre completo',
-            keyboardType: TextInputType.name,
-            onChanged: (value) => ref
-                .read(formLogicRegisterProvider.notifier)
-                .onFullNameChange(value),
-            errorText: formLogicStateRegister.fullName.errorMessage,
-          ),
-          CustomInputComponentShared(
-            icon: Icons.email,
-            placeholder: 'Correo electrónico',
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => ref
-                .read(formLogicRegisterProvider.notifier)
-                .onEmailChange(value),
-            errorText: formLogicStateRegister.email.errorMessage,
-          ),
-          CustomInputComponentShared(
-            icon: Icons.key,
-            placeholder: 'Contraseña',
-            keyboardType: TextInputType.text,
-            isPassword: true,
-            onChanged: (value) => ref
-                .read(formLogicRegisterProvider.notifier)
-                .onPasswordChange(value),
-            errorText: formLogicStateRegister.password.errorMessage,
-          ),
-          ElevatedButtonComponentAuthentication(
-            label: 'Registrarse',
-            onPressed: formLogicStateRegister.isPosting
-                ? null
-                : () async {
-                    FocusScope.of(context).unfocus();
+    return Column(
+      children: [
+        CustomInputComponentShared(
+          icon: Icons.person,
+          placeholder: 'Nombre completo',
+          keyboardType: TextInputType.name,
+          onChanged: (value) => ref
+              .read(formLogicRegisterProvider.notifier)
+              .onFullNameChange(value),
+          errorText: formLogicStateRegister.fullName.errorMessage,
+        ),
+        CustomInputComponentShared(
+          icon: Icons.email,
+          placeholder: 'Correo electrónico',
+          keyboardType: TextInputType.emailAddress,
+          onChanged: (value) =>
+              ref.read(formLogicRegisterProvider.notifier).onEmailChange(value),
+          errorText: formLogicStateRegister.email.errorMessage,
+        ),
+        CustomInputComponentShared(
+          icon: Icons.key,
+          placeholder: 'Contraseña',
+          keyboardType: TextInputType.text,
+          isPassword: true,
+          onChanged: (value) => ref
+              .read(formLogicRegisterProvider.notifier)
+              .onPasswordChange(value),
+          errorText: formLogicStateRegister.password.errorMessage,
+        ),
+        ElevatedButtonComponentAuthentication(
+          label: 'Registrarse',
+          onPressed: formLogicStateRegister.isPosting
+              ? null
+              : () async {
+                  FocusScope.of(context).unfocus();
 
-                    (bool, String) result = await ref
-                        .read(formLogicRegisterProvider.notifier)
-                        .onFormSubmit();
+                  (bool, String) result = await ref
+                      .read(formLogicRegisterProvider.notifier)
+                      .onFormSubmit();
 
-                    if (result.$1) {
-                      if (context.mounted) {
-                        context.go(UsersViewHome.link);
-                      }
-                    } else {
-                      if (context.mounted) {
-                        showAlertDialogShared(context,
-                            title: 'Error al realizar el Registro',
-                            subTitle: result.$2);
-                      }
+                  if (result.$1) {
+                    if (context.mounted) {
+                      context.go(UsersViewHome.link);
                     }
-                    //formLogicStateLogin.toString();
-                  },
-          ),
-          const SizedBox(height: 60),
-          LabelsComponentAuthentication(
-            title: '¿Tienes una cuenta?',
-            subTitle: 'Ingresa ahora!',
-            link: LoginViewAuthentication.link,
-          ),
-        ],
-      ),
+                  } else {
+                    if (context.mounted) {
+                      showAlertDialogShared(context,
+                          title: 'Error al realizar el Registro',
+                          subTitle: result.$2);
+                    }
+                  }
+                  //formLogicStateLogin.toString();
+                },
+        ),
+        const SizedBox(height: 60),
+        LabelsComponentAuthentication(
+          title: '¿Tienes una cuenta?',
+          subTitle: 'Ingresa ahora!',
+          link: LoginViewAuthentication.link,
+        ),
+      ],
     );
   }
 }
